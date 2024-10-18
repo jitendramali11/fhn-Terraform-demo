3d706f081e3f3f434e31ee5383d4c4bc5a9f0c4c
Jitendra Mali <jitendramali11@gmail.com>
2024-10-18 11:18:26 +0530

    third commit

--- modules/storage-account/main.tf
+++ modules/storage-account/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_storage_account" "storage_account" {
   name                     = var.storageaccountname
   resource_group_name      = var.resource_group_name
   location                 = var.location
-  account_tier             = var.account_tier
+  account_tier             = "Standard"
   account_replication_type = "GRS"
 
   tags = {
