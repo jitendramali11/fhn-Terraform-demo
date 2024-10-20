9d82773f45311ffee281846d533ffed3de4d64d8
Jitendra Mali <jitendramali11@gmail.com>
2024-10-20 23:00:49 +0530

    updated

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -40,14 +40,3 @@ variable "app_service_name" {
 variable "web_app_name" {
    type = string
 }
-
-resource_group_name = "dev-fhntestapp-rg"
-location = "North Europe"
-keyvaultname = "devfhntestkv"
-storageaccountname = "devfhnteststr"
-account_tier = "Standard"
-sqlserver_name = "dev-fhntest-sql-server"
-sql_db_name = "dev-fhntest-testdb"
-sku_name = "S0"
-app_service_name = "dev-fhntest-app-serviceplan"
-web_app_name = "dev-fhntest-webapp"
