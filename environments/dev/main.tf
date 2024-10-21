89b1cd5d703bb20933742a06316d93d517c25997
Jitendra Mali <jitendramali11@gmail.com>
2024-10-21 14:28:50 +0530

    third commit

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,18 +1,18 @@
 resource "azurerm_resource_group" "rgname" {
-  name = "dev-fhntestapp-rg"
+  name = "dev-fhntestapp-rg8585"
   location = "North Europe"
 }
 
 module "key_vault" {
   source = "../../modules/keyvault"
-  keyvaultname =  "devfhntestkv"
+  keyvaultname =  "devfhntestkv85"
   resource_group_name = azurerm_resource_group.rgname.name
   location = azurerm_resource_group.rgname.location
 }
 
 module "storage_account" {
   source = "../../modules/storage-account"
-  storageaccountname = "devfhnteststr"
+  storageaccountname = "devfhnteststr85"
   account_tier = "Standard"      
   resource_group_name = azurerm_resource_group.rgname.name
   location = azurerm_resource_group.rgname.location
@@ -21,32 +21,30 @@ module "storage_account" {
 
 module "sql_server" {
   source = "../../modules/sql-server"
-  sqlserver_name = "dev-fhntest-sql-server"
-  sql_db_name = "dev-fhntest-testdb"
+  sqlserver_name = "dev-fhntest-sql-server85"
   resource_group_name= azurerm_resource_group.rgname.name
   location = azurerm_resource_group.rgname.location
 }
 
 module "sql_db" {
   source = "../../modules/sql-server"
-  sqlserver_name = "dev-fhntest-sql-server"
-  sql_db_name = "dev-fhntest-testdb"
+  sql_db_name = "dev-fhntest-testdb85"
   resource_group_name= azurerm_resource_group.rgname.name
   sku_name = "S0"
 } 
 
 module "app_service_plan" {
   source = "../../modules/web-app"
-  app_service_name = "dev-fhntest-app-serviceplan"
-  web_app_name = "dev-fhntest-webapp1"
+  app_service_name = "dev-fhntest-app-serviceplan85"
+  web_app_name = "dev-fhntest-webapp185"
   resource_group_name= azurerm_resource_group.rgname.name
   location = azurerm_resource_group.rgname.location
 } 
 
 module "web_app" {
   source = "../../modules/web-app"
-  app_service_name = "dev-fhntest-app-serviceplan"
-  web_app_name = "dev-fhntest-webapp1"
+  app_service_name = "dev-fhntest-app-serviceplan85"
+  web_app_name = "dev-fhntest-webapp185"
   resource_group_name= azurerm_resource_group.rgname.name
   location = azurerm_resource_group.rgname.location
 } 
