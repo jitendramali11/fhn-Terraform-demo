5aff36379ae1a8f6ea0148aead2fa63049697888
Jitendra Mali <jitendramali11@gmail.com>
2024-10-21 14:59:55 +0530

    third commit

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,18 +1,18 @@
 resource "azurerm_resource_group" "rgname" {
-  name = "dev-fhntestapp-rg85855"
+  name = "dev-fhntestapp-rg85855-fhn"
   location = "North Europe"
 }
 
 module "key_vault" {
   source = "../../modules/keyvault"
-  keyvaultname =  "devfhntestkv855"
+  keyvaultname =  "devfhntestkv855f"
   resource_group_name = azurerm_resource_group.rgname.name
   location = azurerm_resource_group.rgname.location
 }
 
 module "storage_account" {
   source = "../../modules/storage-account"
-  storageaccountname = "devfhnteststr855"
+  storageaccountname = "devfhnteststr85f"
   account_tier = "Standard"      
   resource_group_name = azurerm_resource_group.rgname.name
   location = azurerm_resource_group.rgname.location
@@ -22,23 +22,23 @@ module "storage_account" {
 module "sql_server" {
   source = "../../modules/sql-server"
   sqlserver_name = "dev-fhntest-sql-server855"
-  sql_db_name = "dev-fhntest-testdb85"
+  sql_db_name = "dev-fhntest-testdb85f"
   resource_group_name= azurerm_resource_group.rgname.name
   location = azurerm_resource_group.rgname.location
 }
 
 module "app_service_plan" {
   source = "../../modules/web-app"
-  app_service_name = "dev-fhntest-app-serviceplan855"
-  web_app_name = "dev-fhntest-webapp1855"
+  app_service_name = "dev-fhntest-app-serviceplan855f"
+  web_app_name = "dev-fhntest-webapp1855f"
   resource_group_name= azurerm_resource_group.rgname.name
   location = azurerm_resource_group.rgname.location
 } 
 
 module "web_app" {
   source = "../../modules/web-app"
-  app_service_name = "dev-fhntest-app-serviceplan85"
-  web_app_name = "dev-fhntest-webapp1855"
+  app_service_name = "dev-fhntest-app-serviceplan85f"
+  web_app_name = "dev-fhntest-webapp1855ff"
   resource_group_name= azurerm_resource_group.rgname.name
   location = azurerm_resource_group.rgname.location
 } 
